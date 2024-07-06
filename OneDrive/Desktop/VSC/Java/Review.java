ae0b4ba2fc353bceafd1d086dbc5790d09a10fc4
Grant <5008418345@student.csn.edu>
2024-07-06 11:26:24 -0700

    Adding review.java for 4th try

---
+++ OneDrive/Desktop/VSC/Java/Review.java
@@ -0,0 +1,8 @@
+//This is a simple Java program that prints a message to the console
+public class Review {
+  public static void main(String[] args) {
+    //The main method executes the tasks of the class
+    System.out.println("testing- hello, my first Java is VSCODE!");    
+  }
+}
+
